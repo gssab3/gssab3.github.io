d07de00511ab6705b121e6d3c8a86ca0905891e6
gssab3 <162311328+gssab3@users.noreply.github.com>
2025-12-07 00:06:22 +0100

    Portfolio update (little update)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home: React.FC = () => {
   return (
     <div className="page home-page">
-      <h1>Welcome to your Portfolio</h1>
+      <h1>Welcome to my Portfolio</h1>
       <p>This is the home page. Click on the navigation links above to explore different sections.</p>
     </div>
   )
